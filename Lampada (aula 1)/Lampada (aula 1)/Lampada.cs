7a0980c244ddfa7f11fc77cba5c2c11f5658c3dc
Carolina Libarino <124791376+carolinalibarino@users.noreply.github.com>
2024-02-29 12:37:01 -0300

    Add files via upload

---
+++ Lampada (aula 1)/Lampada (aula 1)/Lampada.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lampada__aula_1_
+{
+    public class Lampada
+    {
+        private bool Ligada { get; set; }
+        private string? Cor { get; set; }
+        private int Potencia { get; set; }
+
+
+        public void ligar()
+        {
+            Ligada = true;
+        }
+
+        public void desligada()
+        {
+            Ligada = false;
+        }
+
+        public bool estaligada()
+        {
+            return Ligada;
+        }
+
+        //testando
+
+        public string? getCor()
+        {
+            return Cor;
+        }
+
+    }
+}
---
+++ Lampada (aula 1)/Lampada (aula 1)/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Lampada__aula_1_
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            //instancia de um objeto:
+            Lampada lamp1 = new Lampada();
+
+            //Chamado o método para ligar a lamp1
+            lamp1.ligar();
+
+            //Chamando o método para verificar se lamp1 está ligada
+            Console.WriteLine("A lâmpada está ligada? " + lamp1.estaligada());
+
+
+            lamp1.desligada();
+            Console.WriteLine("A lampada esta ligada?" + lamp1.estaligada());
+
+
+            //testando
+
+            lamp1.getPotencia = 100;
+            lamp1.getCor = "Azul";
+
+            Console.WriteLine("A potencia da lâmpada é: " + lamp1.getPotencia);
+            Console.WriteLine("A cor da lâmpada é: " + lamp1.getCor);
+
+        }
+
+    }
+}
