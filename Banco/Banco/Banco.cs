8b4e21d8344a0f597247a4e3a8a1fe91ef24abbd
Carolina Libarino <124791376+carolinalibarino@users.noreply.github.com>
2024-03-14 12:36:43 -0300

    Add files via upload

---
+++ Banco/Banco/Banco.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    public class Conta
+    {
+        private float saldo_limite;
+
+        private string? Num_Conta { get; set; }
+        private float Saldo { get; set; }
+        private float Limite { get; set; }
+
+
+        public float consulta_saldo()
+        {
+            return Saldo;
+        }
+
+
+        public void deposito(float valor_deposito)
+        {
+            Saldo += valor_deposito;
+            Console.WriteLine("Seu saldo é de R$" + consulta_saldo());
+        }
+
+
+        public void sacar (float valor_saque)
+        {
+            if(valor_saque > saldo_limite)
+            {
+                Console.WriteLine("Seu saldo não é suficiente");
+            }
+
+            else{
+                Saldo -= valor_saque;
+                Console.WriteLine("Seu saldo é de R$" + consulta_saldo());
+            }
+
+        }
+
+        public void ajustar_limite(float valor_limite)
+        {
+            Limite = valor_limite;
+            Console.WriteLine("Seu limite é de R$" + Limite);
+            saldo_limite = Limite + Saldo;
+            Console.WriteLine("Seu saldo+limite é de R$" + saldo_limite);
+
+        }
+
+    }
+}
+
---
+++ Banco/Banco/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Banco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BANCO MCM");
+
+            Conta conta = new Conta();
+            Console.WriteLine("Seu saldo inicial é de R$" + conta.consulta_saldo());
+
+            conta.deposito(500);
+
+            //Console.WriteLine("Seu novo saldo é de: R$" + conta.consulta_saldo());
+
+            //conta.sacar(1000);
+
+            conta.ajustar_limite(500);
+
+
+        }
+    }
+}
