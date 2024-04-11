c12fc9a68228206accac0d9bd42f5a83349d5a25
Carolina Libarino <124791376+carolinalibarino@users.noreply.github.com>
2024-04-11 13:57:02 -0300

    Add files via upload

---
+++ Lampada (aula 1)/Aviao/Aviao/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Aviao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aviao Aviao = new Aviao();
+
+            Aviao.Compania = "Azul";
+            Aviao.Modelo = "Airbus A330";
+            Aviao.Acelerar(40);
+            Aviao.Cima(12500);
+
+            Console.WriteLine("Senhores passageiros, muito obrigada por escolher a compania " + Aviao.Compania +
+                ", estamos voando no avião " + Aviao.Modelo + ". Nosso avião esta a " + Aviao.Altura +
+                " metros, em uma velocidade de " + Aviao.Velocidade + " km/h");
+            Console.WriteLine("Uma otima viagem a todos!");
+
+        }
+
+    }
+
+}
