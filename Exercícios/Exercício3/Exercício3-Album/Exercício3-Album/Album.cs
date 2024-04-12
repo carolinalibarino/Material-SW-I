a1d829f92f0b18631af3b813d9d15bde66d3a30b
Carolina Libarino <124791376+carolinalibarino@users.noreply.github.com>
2024-04-12 11:40:50 -0300

    Add files via upload

---
+++ Exercícios/Exercício3/Exercício3-Album/Exercício3-Album/Album.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício3_Album
+{
+    internal class Album
+    {
+        public double NumeroFotos { get; set; }
+        public double FotosPagina { get; set; }
+        public double Restante { get; set; }
+
+        public double Div {  get; set; }
+
+        public string NumeroDePaginas()
+        {
+
+            Console.WriteLine("Digite quantas fotos você vai usar");
+            NumeroFotos = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite quantas fotos por página você quer");
+            FotosPagina = Convert.ToDouble(Console.ReadLine());
+
+            Restante = NumeroFotos % FotosPagina;
+            Div = (NumeroFotos / FotosPagina) - 0.5;
+
+            if (Restante == 1)
+            {
+                return "Você pode colocar " + Div + " e vai sobrar " + Restante + " fotos";
+            }
+            else
+            {
+                return "Você pode colocar " + NumeroFotos / FotosPagina + " fotos por página";
+            }
+        }
+    }
+}
---
+++ Exercícios/Exercício3/Exercício3-Album/Exercício3-Album/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercício3_Album
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculando número de fotos por página");
+            Console.WriteLine("");
+
+            Album album = new Album();
+
+            Console.WriteLine(album.NumeroDePaginas());
+
+        }
+    }
+}
