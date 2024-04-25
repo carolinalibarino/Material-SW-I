c993de6e623ee09beb17728839ce814c2dc05e8e
Carolina Libarino <124791376+carolinalibarino@users.noreply.github.com>
2024-04-25 12:31:13 -0300

    Add files via upload

---
+++ ExemploConstrutor/ExemploConstrutor/Cliente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploConstrutor
+{
+    internal class Cliente
+    {
+        public string? Nome { get; set; }
+
+        public string? Email { get; set; }
+
+        public int Senha { get;set; }
+
+
+        public Cliente(string nomeConstrutor, int senhaConstrutor)
+        {
+            Nome= nomeConstrutor;
+            Senha= senhaConstrutor;
+        }
+
+        public void MostraDados()
+        {
+            Console.WriteLine("Nome: " + Nome);
+            Console.WriteLine("Email: " + Email);
+            Console.WriteLine("Senha: " + Senha);
+        }
+    }
+}
---
+++ ExemploConstrutor/ExemploConstrutor/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ExemploConstrutor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo construtor de uma classe");
+
+            Cliente xyz = new Cliente("Carolina", 1234);
+
+            //xyz.Nome = "Carolina";
+            //xyz.Email = "carollibarino2@gmail.com";
+            //xyz.Senha = 1234;
+
+            xyz.MostraDados();
+        }
+    }
+}
